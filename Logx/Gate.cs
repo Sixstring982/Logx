b00140d9de2dd12d43687cea1e6b453330c5e702
Trent Small <Sixstring982@gmail.com>
2012-02-07 19:05:52 -0700

    Basic Logx interface complete

---
+++ Logx/Gate.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Logx
+{
+    abstract class Gate
+    {
+        public Point location;
+        public Gate[] inputs;
+        public abstract bool Output();
+        protected int renderCode = 0;
+        public bool HasInput = true;
+        private bool EvaluatedOutput = false;
+
+        public static Bitmap[] images;
+
+        public void Evaluate()
+        {
+            EvaluatedOutput = Output();
+        }
+
+        public Gate(int x, int y)
+        {
+            location.X = x;
+            location.Y = y;
+        }
+
+        public void Render(Graphics g, bool active = false)
+        {
+            g.DrawImage(images[renderCode], location.X, location.Y);
+            if (EvaluatedOutput)
+                g.FillRectangle(new SolidBrush(Color.Green), new Rectangle(location.X, location.Y, 10, 10));
+            if (active) g.DrawEllipse(new Pen(new SolidBrush(Color.Red)), new Rectangle(location.X + 13, location.Y + 13, 6, 6));
+        }
+
+        public static void LoadImages(string filename)
+        {
+            Bitmap tileSheet = new Bitmap(filename);
+            images = new Bitmap[256];
+            for (int y = 0; y < 16; y++)
+            {
+                for (int x = 0; x < 16; x++)
+                {
+                    images[y * 16 + x] = new Bitmap(32, 32);
+                    Graphics g = Graphics.FromImage(images[y * 16 + x]);
+                    g.DrawImage(tileSheet, new Rectangle(0, 0, 32, 32), new Rectangle(x * 32, y * 32, 32, 32), GraphicsUnit.Pixel);
+                }
+            }
+        }
+    }
+
+    class AndGate : Gate
+    {
+        public AndGate(int x, int y) : base(x, y)
+        {
+            inputs = new Gate[] { null, null };
+            renderCode = 0;
+        }
+
+        public override bool Output()
+        {
+            if(inputs[0] != null && inputs[1] != null)
+                return inputs[0].Output() && inputs[1].Output();
+            return false;
+        }
+    }
+
+    class OrGate : Gate
+    {
+        public OrGate(int x, int y)
+            : base(x, y)
+        {
+            inputs = new Gate[] { null, null };
+            renderCode = 1;
+        }
+
+        public override bool Output()
+        {
+            if (inputs[0] != null && inputs[1] != null)
+                return inputs[0].Output() || inputs[1].Output();
+            return false;
+        }
+    }
+
+    class XorGate : Gate
+    {
+        public XorGate(int x, int y)
+            : base(x, y)
+        {
+            inputs = new Gate[] { null, null };
+            renderCode = 5;
+        }
+
+        public override bool Output()
+        {
+            if (inputs[0] != null && inputs[1] != null)
+                return inputs[0].Output() ^ inputs[1].Output();
+            return false;
+        }
+    }
+
+    class OnGate : Gate
+    {
+        public OnGate(int x, int y) : base(x, y)
+        {
+            inputs = new Gate[] { null, null };
+            HasInput = false;
+            renderCode = 2;
+        }
+
+        public override bool Output()
+        {
+            return true;
+        }
+    }
+
+    class OffGate : Gate
+    {
+        public OffGate(int x, int y)
+            : base(x, y)
+        {
+            inputs = new Gate[] { null, null };
+            HasInput = false;
+            renderCode = 6;
+        }
+
+        public override bool Output()
+        {
+            return false;
+        }
+    }
+
+    class ButtonGate : Gate
+    {
+        private bool On = true;
+        public ButtonGate(int x, int y)
+            : base(x, y)
+        {
+            inputs = new Gate[] { null, null };
+            HasInput = false;
+            renderCode = 3;
+        }
+
+        public void Toggle()
+        {
+            On = !On;
+            if (renderCode == 3) renderCode = 4;
+            else renderCode = 3;
+        }
+
+        public override bool Output()
+        {
+            return On;
+        }
+    }
+}
--- Logx/LogxMainForm.cs
+++ Logx/LogxMainForm.cs
@@ -12,15 +12,38 @@ namespace Logx
     class LogxMainForm : Form
     {
         private Bitmap field;
-        private MenuStrip menuStrip;
         private int screenWidth = 512;
         private int screenHeight = 512;
         private bool running = true;
+        private MouseState currentMS;
+        private MouseState prevMS;
+        private int selectedGate = -1;
+        private int pickedGate = -1;
+        private Point pickedOffset = Point.Empty;
+        private GateTie wiringAnchor = null;
+        private bool ShiftHeld = false, CtrlHeld = false;
+
+        private Pen YellowPen = new Pen(new SolidBrush(Color.Yellow));
+        private Pen BlackPen = new Pen(new SolidBrush(Color.Black));
+
+        class GateTie
+        {
+            public Gate gateptr;
+            public int inputNum;
+
+            public static GateTie Empty = new GateTie();
+        }
+
+        private List<Gate> gateList = new List<Gate>();
+
+        private Font debugFont = new Font("Courier", 12.0f);
 
         public LogxMainForm()
         {
+            MouseState.Setup();
+            prevMS = MouseState.GetMouseState();
+            Gate.LoadImages("Content\\TileSheet.png");
             SetupField();
-            this.Controls.Add(menuStrip);
             this.ClientSize = field.Size;
             this.MaximumSize = this.Size;
             this.MinimumSize = this.Size;
@@ -28,8 +51,86 @@ namespace Logx
             this.SizeGripStyle = SizeGripStyle.Hide;
             this.SetStyle(ControlStyles.Opaque, true);
             this.Paint += new PaintEventHandler(LogxMainForm_Paint);
+            this.MouseMove += new MouseEventHandler(LogxMainForm_MouseMove);
+            this.MouseUp += new MouseEventHandler(LogxMainForm_MouseUp);
+            this.MouseDown += new MouseEventHandler(LogxMainForm_MouseDown);
+            this.KeyDown += new KeyEventHandler(LogxMainForm_KeyDown);
+            this.KeyUp += new KeyEventHandler(LogxMainForm_KeyUp);
             this.Show();
             Run();
+        }
+
+        void LogxMainForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.ShiftKey)
+            {
+                ShiftHeld = false;
+            }
+            if (e.KeyData == (Keys.LButton | Keys.ShiftKey))
+            {
+                CtrlHeld = false;
+            }
+        }
+
+        void LogxMainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode >= Keys.D0 && e.KeyCode <= Keys.D9)
+            {
+                switch (e.KeyCode - Keys.D0)
+                {
+                    case 0:
+                        gateList.Add(new AndGate(screenWidth / 2, screenHeight / 2));
+                        break;
+                    case 1:
+                        gateList.Add(new OrGate(screenWidth / 2, screenHeight / 2));
+                        break;
+                    case 2:
+                        gateList.Add(new OnGate(screenWidth / 2, screenHeight / 2));
+                        break;
+                    case 3:
+                        gateList.Add(new OffGate(screenWidth / 2, screenHeight / 2));
+                        break;
+                    case 4:
+                        gateList.Add(new ButtonGate(screenWidth / 2, screenHeight / 2));
+                        break;
+                    case 5:
+                        gateList.Add(new XorGate(screenWidth / 2, screenHeight / 2));
+                        break;
+                }
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                EvaluateCircuit();
+            }
+            if (e.KeyData == (Keys.ShiftKey | Keys.Shift))
+            {
+                ShiftHeld = true;
+            }
+            if (e.KeyData == (Keys.LButton | Keys.ShiftKey | Keys.Control))
+            {
+                CtrlHeld = true;
+            }
+        }
+
+        private void EvaluateCircuit()
+        {
+            for (int i = 0; i < gateList.Count; i++)
+                gateList[i].Evaluate();
+        }
+
+        void LogxMainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            MouseState.UpdateCMSDown(e);
+        }
+
+        void LogxMainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            MouseState.UpdateCMSUp(e);
+        }
+
+        void LogxMainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            MouseState.UpdateCMSPos(e.X, e.Y);
         }
 
         private void Run(double fps = 60.0)
@@ -38,23 +139,143 @@ namespace Logx
             while (running)
             {
                 startOfFrame = DateTime.Now;
-                Update();
+                FrameUpdate();
                 if ((DateTime.Now - startOfFrame).TotalMilliseconds < (1000 / fps))
                     Thread.Sleep((int)((1000 / fps) - (DateTime.Now - startOfFrame).TotalMilliseconds));
-                Render();
+                FrameRender();
                 Application.DoEvents();
                 if (this.IsDisposed) running = false;
             }
         }
 
-        private void Update()
+        private void FrameUpdate()
         {
+            currentMS = MouseState.GetMouseState();
 
+            if (currentMS.Buttons[MouseButtons.Left] && !prevMS.Buttons[MouseButtons.Left])
+            {
+                for (int i = 0; i < gateList.Count; i++)
+                {
+                    if (gateList[i].location.X < currentMS.X &&
+                        gateList[i].location.X + 32 > currentMS.X &&
+                        gateList[i].location.Y < currentMS.Y &&
+                        gateList[i].location.Y + 32 > currentMS.Y)
+                    {
+                        if (ShiftHeld)
+                        {
+                            gateList.RemoveAt(i);
+                        }
+                        else if (CtrlHeld)
+                        {
+                            if (gateList[i] is ButtonGate)
+                            {
+                                ((ButtonGate)gateList[i]).Toggle();
+                                EvaluateCircuit();
+                            }
+                        }
+                        else
+                        {
+                            pickedOffset.X = currentMS.X - gateList[i].location.X;
+                            pickedOffset.Y = currentMS.Y - gateList[i].location.Y;
+                            pickedGate = i;
+                            selectedGate = i;
+                        }
+                        break;
+                    }
+                    else
+                        pickedGate = -1;
+                }
+            }
+            if (!currentMS.Buttons[MouseButtons.Left] && prevMS.Buttons[MouseButtons.Left])
+            {
+                pickedGate = -1;
+            }
+
+            if (pickedGate != -1)
+            {
+                gateList[pickedGate].location.X = currentMS.X - pickedOffset.X;
+                gateList[pickedGate].location.Y = currentMS.Y - pickedOffset.Y;
+            }
+
+            if (currentMS.Buttons[MouseButtons.Right] && !prevMS.Buttons[MouseButtons.Right])
+            {
+                for (int i = 0; i < gateList.Count; i++)
+                {
+                    if (currentMS.X > gateList[i].location.X + 5 &&
+                        currentMS.X < gateList[i].location.X + 15) //Selected Input Block
+                    {
+                        if (gateList[i].HasInput)
+                        {
+                            if (currentMS.Y > gateList[i].location.Y &&
+                                currentMS.Y < gateList[i].location.Y + 16) //Selected Top Input
+                            {
+                                if (wiringAnchor == null)
+                                {
+                                    wiringAnchor = new GateTie();
+                                    wiringAnchor.inputNum = 0;
+                                    wiringAnchor.gateptr = gateList[i];
+                                }
+                            }
+                            else if (currentMS.Y > gateList[i].location.Y + 16 &&
+                                currentMS.Y < gateList[i].location.Y + 32) //Selected Bottom Input
+                            {
+                                if (wiringAnchor == null)
+                                {
+                                    wiringAnchor = new GateTie();
+                                    wiringAnchor.inputNum = 1;
+                                    wiringAnchor.gateptr = gateList[i];
+                                }
+                            }
+                        }
+                    }
+                    if (currentMS.X >= gateList[i].location.X + 18 &&
+                        currentMS.X <= gateList[i].location.X + 23 &&
+                        currentMS.Y >= gateList[i].location.Y &&
+                        currentMS.Y <= gateList[i].location.Y + 32) //Selected Output Block
+                    {
+                        if (wiringAnchor != null)
+                        {
+                            if (wiringAnchor.gateptr != gateList[i])
+                            {
+                                wiringAnchor.gateptr.inputs[wiringAnchor.inputNum] = gateList[i];
+                                wiringAnchor = null;
+                            }
+                        }
+                    }
+                }
+            }
+
+            prevMS = MouseState.GetMouseState();
         }
 
-        private void Render()
+        private void FrameRender()
         {
-            FillScreen(Color.BlanchedAlmond);
+            FillScreen(Color.DarkGray);
+            Graphics g = Graphics.FromImage(field);
+            for (int i = 0; i < gateList.Count; i++)
+            {
+                if (i == selectedGate)
+                    gateList[i].Render(g, true);
+                else
+                    gateList[i].Render(g);
+                for (int j = 0; j < gateList[i].inputs.Length; j++)
+                {
+                    if (gateList[i].inputs[j] != null)
+                    {
+                        g.DrawLine(BlackPen, new Point(gateList[i].location.X + 5, gateList[i].location.Y + 10 + (10*j)),
+                            new Point(gateList[i].inputs[j].location.X + 22, gateList[i].inputs[j].location.Y + 16));
+                    }
+                }
+            }
+
+            if (wiringAnchor != null)
+            {
+                g.DrawLine(YellowPen, new Point(wiringAnchor.gateptr.location.X + 5, wiringAnchor.gateptr.location.Y + 10 + (10 * wiringAnchor.inputNum)),
+                    new Point(currentMS.X, currentMS.Y));
+            }
+
+            g.DrawString("( " + currentMS.X + ", " + currentMS.Y + ")", debugFont, new SolidBrush(Color.Black), new PointF(0.0f, 490.0f));
+            this.Invalidate();
         }
 
         private void FillScreen(Color c)
